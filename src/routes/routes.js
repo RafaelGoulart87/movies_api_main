1c73392563af7c0aa6796f7820bea664271e7e4f
RafaelGoulart87 <goulart.rafaelrosa89@gmail.com>
2022-03-26 23:48:00 -0300

    first_commit_project

---
+++ src/routes/routes.js
@@ -0,0 +1,30 @@
+import userRoutes from "./users-routes.js";
+import authRoutes from "./auth-routes.js";
+import moviesRoutes from "./movies-routes.js";
+
+export const renderRoutes = [
+  {
+    method: "GET",
+    url: "/health",
+    handler: (_, res) => {
+      res.status(200).send();
+    },
+  },
+  ...Object.values(userRoutes),
+  ...Object.values(authRoutes),
+  ...Object.values(moviesRoutes),
+];
+
+export default (fastify, opts, next) => {
+  fastify.decorateRequest("user", null);
+
+  fastify.addHook("onRequest", (req, res, next) => {
+    console.log("onRequest");
+    req.user = null;
+    next();
+  });
+  for (let route of renderRoutes) {
+    fastify.route(route);
+  }
+  next();
+};
